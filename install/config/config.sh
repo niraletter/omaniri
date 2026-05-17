7caf3e3329b64bf808856dde22f7ef4e2f1618de
N1R4 <lmntrix.as619@gmail.com>
2026-05-17 17:41:43 +0545

    add path to bash_profile

--- install/config/config.sh
+++ install/config/config.sh
@@ -2,5 +2,6 @@
 mkdir -p ~/.config
 cp -R ~/.local/share/omaniri/config/* ~/.config/
 
-# Use default bashrc from omaniri
+# Use default bashrc and bash_profile from omaniri
 cp ~/.local/share/omaniri/default/bashrc ~/.bashrc
+cp ~/.local/share/omaniri/default/bash_profile ~/.bash_profile
