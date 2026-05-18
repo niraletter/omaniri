ed76a57c1ae3d776e2b663b262abfda6fb8f1191
N1R4 <lmntrix.as619@gmail.com>
2026-05-18 21:17:26 +0545

    fix

--- install/first-run/gnome-theme.sh
+++ install/first-run/gnome-theme.sh
@@ -1,5 +1,6 @@
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
+gsettings set org.gnome.desktop.interface cursor-theme "macOS"
 
 sudo gtk-update-icon-cache /usr/share/icons/Yaru
