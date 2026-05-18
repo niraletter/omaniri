1dd16e791cfd0b5632cafdaffae3fa8ae8aae77a
N1R4 <lmntrix.as619@gmail.com>
2026-05-18 19:49:23 +0545

    Restrict snap-pac operations to upgrades only
    Updated snap-pac hooks to restrict operations to upgrades only.

--- install/first-run/restrict-snap-pac.sh
+++ install/first-run/restrict-snap-pac.sh
@@ -1,4 +1,6 @@
 sudo sed -i '/^Operation = Install$/d; /^Operation = Remove$/d' \
   /usr/share/libalpm/hooks/05-snap-pac-pre.hook \
   /usr/share/libalpm/hooks/zz-snap-pac-post.hook
-echo "Done - snapshots only on Upgrade now"
+sudo sed -i 's|/usr/share/libalpm/scripts/snap-pac post|/bin/true|' \
+  /usr/share/libalpm/hooks/zz-snap-pac-post.hook
+echo "Done - pre snapshots only on Upgrade"
