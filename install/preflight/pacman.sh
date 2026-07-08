71de48b65a552459adf8354fe441afe61a1c6e8e
N1R4 <lmntrix.as619@gmail.com>
2026-07-08 20:47:32 +0545

    Enhance pacman configuration with repo refresh
    Added commands to refresh all repositories after configuring pacman.

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,7 +1,11 @@
 if [[ -n ${OMANIRI_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   omaniri-pkg-add base-devel
-
+  
   # Configure pacman
   sudo cp -f ~/.local/share/omaniri/default/pacman/pacman.conf /etc/pacman.conf
+  sudo pacman -Sy
+
+  # Refresh all repos
+  sudo pacman -Syyuu --noconfirm
 fi  
