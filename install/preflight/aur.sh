13a27209ad27c05cf88ead323aac35df7be81c3d
N1R4 <lmntrix.as619@gmail.com>
2026-07-09 01:13:00 +0545

    Refactor yay installation

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -1,10 +1,7 @@
-if ! command -v yay &>/dev/null; then
-  cd /tmp
-  rm -rf yay-bin
-  git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin
+ if ! command -v yay &>/dev/null; then
+  tmpdir=$(mktemp -d)
+  git clone https://aur.archlinux.org/yay-bin.git "$tmpdir/yay-bin"
+  cd "$tmpdir/yay-bin"
   makepkg -si --noconfirm
-  cd -
-  rm -rf yay-bin
-  cd ~
+  rm -rf "$tmpdir"
 fi
