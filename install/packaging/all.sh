1a280de63ab29fa32f1623aa2f178852aea4c5d8
Nirakar Bogati <lmntrix.as619@gmail.com>
2026-07-09 19:46:11 +0545

    omaniri-pkg-add: hardcode yaru-icon-theme and asusctl for Chaotic-AUR only; restore Chaotic-AUR in pacman.conf and preflight

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,5 +1,4 @@
 run_logged $OMANIRI_INSTALL/packaging/base.sh
-run_logged $OMANIRI_INSTALL/packaging/chaotic.sh
 run_logged $OMANIRI_INSTALL/packaging/fonts.sh
 run_logged $OMANIRI_INSTALL/packaging/icons.sh
 run_logged $OMANIRI_INSTALL/packaging/tuis.sh
--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -2,6 +2,12 @@ if [[ -n ${OMANIRI_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   omaniri-pkg-add base-devel
 
+  # Add Chaotic-AUR for pre-built AUR package binaries
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+
   # Configure pacman
   sudo cp -f ~/.local/share/omaniri/default/pacman/pacman.conf /etc/pacman.conf
 
