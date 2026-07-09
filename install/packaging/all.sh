9fa53e76c50124f9006f9d6ce9bb6f33eb360cc2
Nirakar Bogati <lmntrix.as619@gmail.com>
2026-07-09 19:23:01 +0545

    Restrict Chaotic-AUR to yaru-icon-theme and asusctl only; remove from general pacman.conf

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,4 +1,5 @@
 run_logged $OMANIRI_INSTALL/packaging/base.sh
+run_logged $OMANIRI_INSTALL/packaging/chaotic.sh
 run_logged $OMANIRI_INSTALL/packaging/fonts.sh
 run_logged $OMANIRI_INSTALL/packaging/icons.sh
 run_logged $OMANIRI_INSTALL/packaging/tuis.sh
