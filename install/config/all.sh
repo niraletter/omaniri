860375764a17f14cb7f84f626d3bf33fdad0ec63
N1R4 <lmntrix.as619@gmail.com>
2026-07-10 03:50:01 +0545

    Update all.sh

--- install/config/all.sh
+++ install/config/all.sh
@@ -38,7 +38,6 @@ run_logged $OMANIRI_INSTALL/config/hardware/network.sh
 run_logged $OMANIRI_INSTALL/config/hardware/fix-surface-keyboard.sh
 run_logged $OMANIRI_INSTALL/config/hardware/fix-synaptic-touchpad.sh
 run_logged $OMANIRI_INSTALL/config/hardware/fix-tuxedo-backlight.sh
-run_logged $OMANIRI_INSTALL/config/hardware/fix-yt6801-ethernet-adapter.sh
 run_logged $OMANIRI_INSTALL/config/hardware/set-wireless-regdom.sh
 run_logged $OMANIRI_INSTALL/config/hardware/bluetooth.sh
 run_logged $OMANIRI_INSTALL/config/hardware/usb-autosuspend.sh
