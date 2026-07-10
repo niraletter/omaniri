2c7e26825232823499e64f3d0d7a7ff6dc7ec4f8
N1R4 <lmntrix.as619@gmail.com>
2026-07-10 22:41:46 +0545

    Update all.sh

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,4 +1,4 @@
-run_logged $OMANIRI_INSTALL/packaging/base.sh
+run_logged $OMANIRI_INSTALL/packaging/base.sh || exit 1
 run_logged $OMANIRI_INSTALL/packaging/fonts.sh
 run_logged $OMANIRI_INSTALL/packaging/icons.sh
 run_logged $OMANIRI_INSTALL/packaging/tuis.sh
