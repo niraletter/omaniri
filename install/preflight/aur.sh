5468fb11ee475c0bf81ab9942620ab3fef0a80a9
Nirakar Bogati <lmntrix.as619@gmail.com>
2026-07-11 16:13:36 +0545

    install AUR packages from offline mirror when available

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -1,7 +1,11 @@
- if ! command -v yay &>/dev/null; then
-  tmpdir=$(mktemp -d)
-  git clone https://aur.archlinux.org/yay-bin.git "$tmpdir/yay-bin"
-  cd "$tmpdir/yay-bin"
-  makepkg -si --noconfirm
-  rm -rf "$tmpdir"
+  if ! command -v yay &>/dev/null; then
+  if pacman -Si yay-bin &>/dev/null; then
+    sudo pacman -S --noconfirm yay-bin
+  else
+    tmpdir=$(mktemp -d)
+    git clone https://aur.archlinux.org/yay-bin.git "$tmpdir/yay-bin"
+    cd "$tmpdir/yay-bin"
+    makepkg -si --noconfirm
+    rm -rf "$tmpdir"
+  fi
 fi
