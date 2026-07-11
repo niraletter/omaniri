89851a8af8429d283417c1ba191bf8d1f9b169b4
Nirakar Bogati <lmntrix.as619@gmail.com>
2026-07-11 16:14:39 +0545

    do not attempt internet access during offline ISO install

--- install/preflight/aur.sh
+++ install/preflight/aur.sh
@@ -1,11 +1,14 @@
   if ! command -v yay &>/dev/null; then
   if pacman -Si yay-bin &>/dev/null; then
     sudo pacman -S --noconfirm yay-bin
-  else
+  elif [[ -n ${OMANIRI_ONLINE_INSTALL:-} ]]; then
     tmpdir=$(mktemp -d)
     git clone https://aur.archlinux.org/yay-bin.git "$tmpdir/yay-bin"
     cd "$tmpdir/yay-bin"
     makepkg -si --noconfirm
     rm -rf "$tmpdir"
+  else
+    echo "yay-bin package not found in the prebuilt offline mirror." >&2
+    exit 1
   fi
 fi
